7fe6489596790dc9436790a405d7990649e540eb
Ana <apol@corunet.com>
2021-09-15 18:32:57 +0200

    fix(header): fix nav bar styles

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -23,7 +23,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -37,14 +37,14 @@ export default function Header() {
   return (
     <HeaderStyles>
       <div className="bar">
-      <Logo>
-        <Link href="/">Sick fits</Link>
-      </Logo>
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
+        <Nav />
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
