553c575af975c675d4efa4bbcad0768516489296
Ana <apol@corunet.com>
2021-09-15 20:55:21 +0200

    feat(sell): send create new product request to database

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,22 +1,61 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
+
+const CREATE_PRODUCT_MUTATION_QUERY = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+    }
+  }
+`;
 
 export default function CreateProduct() {
-  const { inputs, handleChange } = useForm({
+  const { inputs, handleChange, clearForm } = useForm({
     image: '',
     name: 'Nice shoes',
     price: 213,
     description: 'There are the best shoes!',
   });
 
-  function handleSubmit(e) {
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION_QUERY,
+    {
+      variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+    }
+  );
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    // TODO store data introduced
+    const res = await createProduct();
+    clearForm();
+    Router.push({
+      pathname: `/product/${res.data.createProduct.id}`,
+    });
   }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input
