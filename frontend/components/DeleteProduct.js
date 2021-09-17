509737fece965fa2999e95c5db2e18eb83da7498
Ana <apol@corunet.com>
2021-09-17 15:08:32 +0200

    fix(product): remove products from cache after deletion

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -10,13 +10,15 @@ const DELETE_PRODUCT_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
 export default function DeleteProduct({ id, children }) {
-  const [deleteProduct, { error, loading }] = useMutation(
-    DELETE_PRODUCT_MUTATION,
-    {
-      variables: { id },
-    }
-  );
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+    update,
+  });
 
   return (
     <button
