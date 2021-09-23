81f3096eae96f0c88d956b8de97812b09b84c476
Ana <apol@corunet.com>
2021-09-23 13:12:53 +0200

    feat(orders): add checkout frontend logic for payment

--- frontend/components/Checkout.js
+++ frontend/components/Checkout.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-import { CardElement, Elements } from '@stripe/react-stripe-js';
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
+import nProgress from 'nprogress';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 import styled from 'styled-components';
+import { useRouter } from 'next/router';
 import SickButton from './styles/SickButton';
+import { useCart } from '../lib/cartState';
+import { CURRENT_USER_QUERY } from './User';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -12,19 +24,81 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
 `;
 
-const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    checkout(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        name
+      }
+    }
+  }
+`;
 
-export default function Checkout() {
-  function handleSubmit(e) {
+function CheckoutForm() {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  const router = useRouter();
+  const { closeCart } = useCart();
+  const [checkout, { error: graphqlError }] = useMutation(
+    CREATE_ORDER_MUTATION,
+    { refetchQueries: [{ query: CURRENT_USER_QUERY }] }
+  );
+
+  async function handleSubmit(e) {
     e.preventDefault();
+    setLoading(true);
+
+    // Start page transition
+    nProgress.start();
+
+    // Create payment method via stripe
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+    if (error) {
+      setError(error);
+      nProgress.done();
+      return;
+    }
+
+    // Send the payment token to the server
+    const order = await checkout({ variables: { token: paymentMethod.id } });
+
+    // Change page after payment
+    router.push({
+      pathname: '/orders',
+      query: { id: order.data.checkout.id },
+    });
+    closeCart();
+
+    setLoading(false);
+    nProgress.done();
   }
 
   return (
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      {error && <p style={{ fontSize: 12 }}>{error.message}</p>}
+      {graphqlError && <p style={{ fontSize: 12 }}>{graphqlError.message}</p>}
+      <CardElement />
+      <SickButton>Check out now</SickButton>
+    </CheckoutFormStyles>
+  );
+}
+
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+
+export default function Checkout() {
+  return (
     <Elements stripe={stripeLib}>
-      <CheckoutFormStyles onSubmit={handleSubmit}>
-        <CardElement />
-        <SickButton>Check out now</SickButton>
-      </CheckoutFormStyles>
+      <CheckoutForm />
     </Elements>
   );
 }
