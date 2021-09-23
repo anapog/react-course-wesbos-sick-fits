e773f5abd72ef2f661bca6324a57c7a91cf36530
Ana <apol@corunet.com>
2021-09-23 14:08:47 +0200

    feat(orders): fix order item quantity storage

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -68,7 +68,7 @@ export default async function checkout(
       name: cartItem.product.name,
       description: cartItem.product.description,
       price: cartItem.product.price,
-      quantity: cartItem.product.quantity,
+      quantity: cartItem.quantity,
       photo: { connect: { id: cartItem.product.photo.id } },
     };
   });
